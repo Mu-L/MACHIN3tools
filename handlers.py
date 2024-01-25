c04e856fbae490ce08f874f55a8f8691e3be018a
machin3io <social@machin3.io>
2024-01-25 16:13:41 +0100

    handlers introduce global and local debug toggles

--- handlers.py
+++ handlers.py
@@ -1,6 +1,7 @@
 import bpy
 import os
 from bpy.app.handlers import persistent
+from time import time
 from . utils.application import delay_execution
 from . utils.draw import draw_axes_HUD, draw_focus_HUD, draw_surface_slide_HUD, draw_screen_cast_HUD
 from . utils.group import select_group_children
@@ -10,6 +11,8 @@ from . utils.registration import get_prefs, reload_msgbus, get_addon
 from . utils.system import get_temp_dir
 from . utils.view import sync_light_visibility
 
+global_debug = False
+# global_debug = True
 
 
 # ------------ DEFERRED EXECUTION ---------------
@@ -20,11 +23,17 @@ axesHUD = None
 prev_axes_objects = []
 
 def manage_axes_HUD():
-    global axesHUD, prev_axes_objects
+    global global_debug, axesHUD, prev_axes_objects
+
+    debug = global_debug
+    debug = False
 
     scene = getattr(bpy.context, 'scene', None)
 
     if scene:
+
+        if debug:
+            print("  axes HUD")
 
         # if you unregister the addon, the handle will somehow stay arround as a capsule object with the following name
         # despite that, the object will return True, and so we need to check for this or no new handler will be created when re-registering
@@ -41,28 +50,36 @@ def manage_axes_HUD():
         if scene.M3.draw_cursor_axes:
             axes_objects.append('CURSOR')
 
-        # print()
-
         if axes_objects:
-            # print("axes objects present")
+            if debug:
+                print("   axes objects present:", [obj if obj == 'CURSOR' else obj.name for obj in axes_objects])
 
             if axes_objects != prev_axes_objects:
-                # print(" axes objects changed")
+                if debug:
+                    print("   axes objects changed")
+
                 prev_axes_objects = axes_objects
 
                 # the objects have changed, remove the previous handler if one exists
                 if axesHUD:
-                    # print("  removing previous draw handler")
+                    if debug:
+                        print("   removing previous draw handler")
+
                     bpy.types.SpaceView3D.draw_handler_remove(axesHUD, 'WINDOW')
 
                 # create a new handler
-                # print("  adding new draw handler")
+
+                if debug:
+                    print("   adding new draw handler")
                 axesHUD = bpy.types.SpaceView3D.draw_handler_add(draw_axes_HUD, (bpy.context, axes_objects), 'WINDOW', 'POST_VIEW')
 
         # remove the handler when no axes objects are present anymore
         elif axesHUD:
             bpy.types.SpaceView3D.draw_handler_remove(axesHUD, 'WINDOW')
-            # print("removing old draw handler")
+
+            if debug:
+                print("   removing old draw handler")
+
             axesHUD = None
             prev_axes_objects = []
 
@@ -72,11 +89,17 @@ def manage_axes_HUD():
 focusHUD = None
 
 def manage_focus_HUD():
-    global focusHUD
+    global global_debug, focusHUD
+
+    debug = global_debug
+    debug = False
 
     scene = getattr(bpy.context, 'scene', None)
 
     if scene:
+
+        if debug:
+            print("  focus HUD")
 
         # if you unregister the addon, the handle will somehow stay arround as a capsule object with the following name
         # despite that, the object will return True, and so we need to check for this or no new handler will be created when re-registering
@@ -87,9 +110,15 @@ def manage_focus_HUD():
 
         if history:
             if not focusHUD:
+                if debug:
+                    print("   adding new draw handler")
+
                 focusHUD = bpy.types.SpaceView3D.draw_handler_add(draw_focus_HUD, (bpy.context, (1, 1, 1), 1, 2), 'WINDOW', 'POST_PIXEL')
 
         elif focusHUD:
+            if debug:
+                print("   removing old draw handler")
+
             bpy.types.SpaceView3D.draw_handler_remove(focusHUD, 'WINDOW')
             focusHUD = None
 
@@ -99,7 +128,13 @@ def manage_focus_HUD():
 surfaceslideHUD = None
 
 def manage_surface_slide_HUD():
-    global surfaceslideHUD
+    global global_debug, surfaceslideHUD
+
+    debug = global_debug
+    debug = False
+
+    if debug:
+        print("  surface slide HUD")
 
     # if you unregister the addon, the handle will somehow stay arround as a capsule object with the following name
     # despite that, the object will return True, and so we need to check for this or no new handler will be created when re-registering
@@ -112,9 +147,15 @@ def manage_surface_slide_HUD():
         surfaceslide = [mod for mod in active.modifiers if mod.type == 'SHRINKWRAP' and 'SurfaceSlide' in mod.name]
 
         if surfaceslide and not surfaceslideHUD:
+            if debug:
+                print("   adding new draw handler")
+
             surfaceslideHUD = bpy.types.SpaceView3D.draw_handler_add(draw_surface_slide_HUD, (bpy.context, (0, 1, 0), 1, 2), 'WINDOW', 'POST_PIXEL')
 
         elif surfaceslideHUD and not surfaceslide:
+            if debug:
+                print("   removing old draw handler")
+
             bpy.types.SpaceView3D.draw_handler_remove(surfaceslideHUD, 'WINDOW')
             surfaceslideHUD = None
 
@@ -124,7 +165,13 @@ def manage_surface_slide_HUD():
 screencastHUD = None
 
 def manage_screen_cast_HUD():
-    global screencastHUD
+    global global_debug, screencastHUD
+
+    debug = global_debug
+    debug = False
+
+    if debug:
+        print("  screen cast HUD")
 
     wm = bpy.context.window_manager
 
@@ -136,9 +183,15 @@ def manage_screen_cast_HUD():
     # if bpy.context.window_manager.operators and scene.M3.screen_cast:
     if getattr(wm, 'M3_screen_cast', False):
         if not screencastHUD:
+            if debug:
+                print("   adding new draw handler")
+
             screencastHUD = bpy.types.SpaceView3D.draw_handler_add(draw_screen_cast_HUD, (bpy.context, ), 'WINDOW', 'POST_PIXEL')
 
     elif screencastHUD:
+        if debug:
+            print("   removing old draw handler")
+
         bpy.types.SpaceView3D.draw_handler_remove(screencastHUD, 'WINDOW')
         screencastHUD = None
 
@@ -146,26 +199,38 @@ def manage_screen_cast_HUD():
 # GROUP
 
 def manage_group():
-    context = bpy.context
+    global global_debug
 
-    scene = getattr(bpy.context, 'scene', None)
+    debug = global_debug
+    debug = False
+
+    if debug:
+        print("  group management")
+
+    C = bpy.context
+    scene = getattr(C, 'scene', None)
+    m3 = scene.M3
 
     # only actually execute any of the group stuff, if there is a 3d view, since we know that already
-    if scene and context.mode == 'OBJECT':
-        active = active if (active := get_active_object(context)) and active.M3.is_group_empty and active.select_get() else None
+    if scene and C.mode == 'OBJECT':
+        active = active if (active := get_active_object(C)) and active.M3.is_group_empty and active.select_get() else None
 
 
         # AUTO SELECT
 
-        if scene.M3.group_select and active:
-            # print(" auto-select")
-            select_group_children(context.view_layer, active, recursive=scene.M3.group_recursive_select)
+        if m3.group_select and active:
+            if debug:
+                print("   auto-selecting")
+
+            select_group_children(C.view_layer, active, recursive=m3.group_recursive_select)
 
 
         # STORE USER-SET EMPTY SIZE
 
         if active:
-            # print(" storing user-set empty size")
+            if debug:
+                print("   storing user-set empty size")
+
             # without this you can't actually set a new empty size, because it would be immediately reset to the stored value, if group_hide is enabled
             if round(active.empty_display_size, 4) != 0.0001 and active.empty_display_size != active.M3.group_size:
                 active.M3.group_size = active.empty_display_size
@@ -173,8 +238,9 @@ def manage_group():
 
         # HIDE / UNHIDE
 
-        if (visible := get_visible_objects(context)) and scene.M3.group_hide:
-            # print(" hide/unhide") 
+        if (visible := get_visible_objects(C)) and m3.group_hide:
+            if debug:
+                print("   hiding/unhiding") 
 
             selected = [obj for obj in visible if obj.M3.is_group_empty and obj.select_get()]
             unselected = [obj for obj in visible if obj.M3.is_group_empty and not obj.select_get()]
@@ -217,13 +283,24 @@ def manage_asset_drop_cleanup():
     NOTE: this may also be related to other handlers (perhaps in HC) causing a second run, so verify that's not the case
     '''
     
-    global was_asset_drop_cleanup_executed
+    global global_debug, was_asset_drop_cleanup_executed
+
+    debug = global_debug
+    debug = False
+
+    if debug:
+        print("  asset drop management")
+
 
     if was_asset_drop_cleanup_executed:
+        if debug:
+            print("   skipping second (duplicate) run")
+
         was_asset_drop_cleanup_executed = False
         return
 
-    # print(" asset drop cleanup")
+    if debug:
+        print("   checking for asset drop cleanup")
 
     global meshmachine, decalmachine
 
@@ -233,8 +310,9 @@ def manage_asset_drop_cleanup():
     if decalmachine is None:
         decalmachine = get_addon('DECALmachine')[0]
 
-    # print("  meshmachine:", meshmachine)
-    # print("  decalmachine:", decalmachine)
+    if debug:
+        print("    meshmachine:", meshmachine)
+        print("    decalmachine:", decalmachine)
 
     context = bpy.context
 
@@ -249,25 +327,33 @@ def manage_asset_drop_cleanup():
 
                 # unlink MESHmachine stashes and DECALmachine decal backups
                 if lastop.bl_idname == 'OBJECT_OT_transform_to_mouse':
-                    # print()
-                    # print("  asset drop detected!")
+                    if debug:
+                        print()
+                        print("    asset drop detected!")
+
                     # start = time.time()
 
                     visible = get_visible_objects(context)
 
                     for obj in visible:
                         if meshmachine and obj.MM.isstashobj:
-                            # print("   STASH!", obj.name)
+                            if debug:
+                                print("     stash object:", obj.name)
 
                             for col in obj.users_collection:
-                                # print(f"    unlinking {obj.name} from {col.name}")
+                                if debug:
+                                    print(f"      unlinking from {col.name}")
+
                                 col.objects.unlink(obj)
 
                         if decalmachine and obj.DM.isbackup:
-                            # print("   DECAL BACKUP!", obj.name)
+                            if debug:
+                                print("     decal backup object:", obj.name)
 
                             for col in obj.users_collection:
-                                # print(f"    unlinking {obj.name} from {col.name}")
+                                if debug:
+                                    print(f"      unlinking from {col.name}")
+
                                 col.objects.unlink(obj)
 
                     # print(f" MACHIN3tools asset drop check done, after {time.time() - start:.20f} seconds")
@@ -278,10 +364,17 @@ def manage_asset_drop_cleanup():
                 # print("material dropped")
 
 
-
 # MANAGE LIGHTS
 
 def manage_lights_decrease_and_visibility_sync():
+    global global_debug
+
+    debug = global_debug
+    debug = False
+
+    if debug:
+        print("  light descrease and visiblity sync")
+
     scene = getattr(bpy.context, 'scene', None)
 
     if scene:
@@ -295,19 +388,30 @@ def manage_lights_decrease_and_visibility_sync():
 
                 # decrease on start of rendering
                 if last in ['NONE', 'INCREASE'] and divider > 1:
-                    # print()
-                    # print("decreasing lights for cycles when starting render")
+                    if debug:
+                        print("   decreasing lights for cycles when starting render")
 
                     m3.adjust_lights_on_render_last = 'DECREASE'
                     m3.is_light_decreased_by_handler = True
 
-                    adjust_lights_for_rendering(mode='DECREASE')
+                    adjust_lights_for_rendering(mode='DECREASE', debug=debug)
 
         if p.activate_render and p.render_sync_light_visibility:
+            if debug:
+                print("   light visibility syncing")
+
             sync_light_visibility(scene)
 
 
 def manage_lights_increase():
+    global global_debug
+    
+    debug = global_debug
+    debug = False
+
+    if debug:
+        print("  light increase")
+
     scene = getattr(bpy.context, 'scene', None)
 
     if scene:
@@ -320,26 +424,31 @@ def manage_lights_increase():
 
                 # increase again when finished
                 if last == 'DECREASE' and m3.is_light_decreased_by_handler:
-                    # print()
-                    # print("increasing lights for cycles when finshing/aborting render")
+                    if debug:
+                        print("   increasing lights for cycles when finshing/aborting render")
 
                     m3.adjust_lights_on_render_last = 'INCREASE'
                     m3.is_light_decreased_by_handler = False
 
-                    adjust_lights_for_rendering(mode='INCREASE')
+                    adjust_lights_for_rendering(mode='INCREASE', debug=debug)
 
 
 # SAVE FILE before UNDO
 
 def pre_undo_save():
+    global global_debug
+
+    debug = global_debug
     debug = False
-    debug = True
+
+    if debug:
+        print("  undo save")
 
     scene = getattr(bpy.context, 'scene', None)
 
     # PRE-UNDO SAVING
 
-    if scene and get_prefs().save_pie_use_undo_save:
+    if scene:
         m3 = scene.M3
 
         if m3.use_undo_save:
@@ -349,8 +458,7 @@ def pre_undo_save():
             bprefs =  bpy.context.preferences
             
             if debug:
-                print()
-                print("active operator:", C.active_operator)
+                print("   active operator:", C.active_operator)
 
             first_redo = False
 
@@ -366,9 +474,9 @@ def pre_undo_save():
                 if temp_dir:
                     if debug:
                         if first_redo:
-                            print("saving before first redo")
+                            print("    saving before first redo")
                         else:
-                            print("saving before undoing")
+                            print("    saving before undoing")
 
                         # print(" to temp folder:", temp_dir)
 
@@ -387,16 +495,15 @@ def pre_undo_save():
                     filepath = os.path.join(temp_dir, name + '_undosave' + ext)
 
                     if debug: 
-                        print(" to temp folder:", filepath)
+                        print("     to temp folder:", filepath)
 
                     if debug:
-                        from time import time
                         start = time()
 
                     bpy.ops.wm.save_as_mainfile(filepath=filepath, check_existing=True, copy=True, compress=True)
 
                     if debug:
-                        print("time:", time() - start)
+                        print("     save time:", time() - start)
 
 
 # ----------------- HANDLERS --------------------
@@ -405,6 +512,13 @@ def pre_undo_save():
 
 @persistent
 def load_post(none):
+    global global_debug
+
+    if global_debug:
+        print()
+        print("MACHIN3tools load post handler:")
+        print(" reloading msgbus")
+
     reload_msgbus()
 
 
@@ -412,36 +526,64 @@ def load_post(none):
 
 @persistent
 def depsgraph_update_post(scene):
-    # print()
-    # print("dg update post handler")
+    global global_debug
+
+    if global_debug:
+        print()
+        print("MACHIN3tools depsgraph update post handler:")
+
+    p = get_prefs()
 
 
     # AXES HUD
 
-    delay_execution(manage_axes_HUD)
+    if p.activate_shading_pie:
+        if global_debug:
+            print(" managing axes HUD")
+
+        delay_execution(manage_axes_HUD)
 
 
     # FOCUS HUD
 
-    delay_execution(manage_focus_HUD)
+    if p.activate_focus:
+        if global_debug:
+            print(" managing focus HUD")
+
+        delay_execution(manage_focus_HUD)
 
 
     # SURFACE SLIDE HUD
 
-    delay_execution(manage_surface_slide_HUD)
+    if p.activate_surface_slide:
+        if global_debug:
+            print(" managing surface slide HUD")
+
+        delay_execution(manage_surface_slide_HUD)
 
 
     # SCREEN CAST HUD
 
-    delay_execution(manage_screen_cast_HUD)
+    if p.activate_save_pie and p.show_screencast:
+        if global_debug:
+            print(" managing screen cast HUD")
+
+        delay_execution(manage_screen_cast_HUD)
 
 
     # GROUP
 
-    delay_execution(manage_group)
+    if p.activate_group:
+        if global_debug:
+            print(" managing group")
+
+        delay_execution(manage_group)
 
 
     # ASSET DROP CLEANUP
+
+    if global_debug:
+        print(" managing asset drop")
 
     delay_execution(manage_asset_drop_cleanup)
 
@@ -450,18 +592,40 @@ def depsgraph_update_post(scene):
 
 @persistent
 def render_start(scene):
+    global global_debug
+
+    if global_debug:
+        print()
+        print("MACHIN3tools render start handler:")
+
+    p = get_prefs()
 
     # LIGHT DESCREASE + VISIBILITY SYNC on RENDER START
 
-    delay_execution(manage_lights_decrease_and_visibility_sync)
+    if p.activate_render and (p.render_adjust_lights_on_render or p.render_enforce_hide_render):
+        if global_debug:
+            print(" managing light decrease and light visibility sync")
+
+        delay_execution(manage_lights_decrease_and_visibility_sync)
 
 
 @persistent
 def render_end(scene):
+    global global_debug
+
+    if global_debug:
+        print()
+        print("MACHIN3tools render cancel or complete handler:")
+
+    p = get_prefs()
 
     # LIGHT INCREASE on RENDER CANCEL/COMPLETE
 
-    delay_execution(manage_lights_increase)
+    if p.activate_render and p.render_adjust_lights_on_render:
+        if global_debug:
+            print(" managing light increase")
+
+        delay_execution(manage_lights_increase)
 
 
 # PRE-UNDO HANDLER
@@ -470,7 +634,18 @@ last_active_operator = None
 
 @persistent
 def undo_pre(scene):
+    global global_debug
+
+    if global_debug:
+        print()
+        print("MACHIN3tools undo pre handler:")
+
+    p = get_prefs()
 
     # PRE-UNDO SAVING
 
-    delay_execution(pre_undo_save)
+    if p.activate_save_pie and p.save_pie_use_undo_save:
+        if global_debug:
+            print(" managing pre undo save")
+
+        delay_execution(pre_undo_save)
--- utils/light.py
+++ utils/light.py
@@ -1,17 +1,22 @@
 import bpy
 
 
-def adjust_lights_for_rendering(mode='DECREASE'):
+def adjust_lights_for_rendering(mode='DECREASE', debug=False):
     divider = bpy.context.scene.M3.adjust_lights_on_render_divider
 
     for light in bpy.data.lights:
         if light.type == 'AREA':
-            print("", light.name, light.energy, ' > ', light.energy / divider)
 
             if mode == 'DECREASE':
+                if debug:
+                    print("   ", light.name, light.energy, ' > ', light.energy / divider)
+
                 light.energy /= divider
 
             elif mode == 'INCREASE':
+                if debug:
+                    print("   ", light.name, light.energy, ' > ', light.energy * divider)
+
                 light.energy *= divider
 
 
